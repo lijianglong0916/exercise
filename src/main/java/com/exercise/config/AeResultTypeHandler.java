1e02bbbd8a624ffb17777fbcb9c5c60fca1df424
lijianglong <jianglong.li@hand-china.com>
2021-01-12 13:37:16 +0800

    [update]拦截select*支持转驼峰

--- src/main/java/com/exercise/config/AeResultTypeHandler.java
+++ src/main/java/com/exercise/config/AeResultTypeHandler.java
@@ -2,9 +2,12 @@ package com.exercise.config;
 
 import org.apache.ibatis.cursor.Cursor;
 import org.apache.ibatis.executor.resultset.ResultSetHandler;
+import org.hzero.core.util.FieldNameUtils;
 
+import java.lang.reflect.Field;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -14,14 +17,29 @@ import java.util.List;
 public class AeResultTypeHandler implements ResultSetHandler {
     @Override
     public <E> List<E> handleResultSets(Statement statement) throws SQLException {
-        ResultSet resultSet = statement.getResultSet();
         List<E> resultSets = new ArrayList<>();
-        PreparedStatement ps=null;
+        PreparedStatement ps = null;
         ResultSetMetaData metaData = ps.getMetaData();
         int columnCount = metaData.getColumnCount();
-        for (int i=0;i<columnCount;i++){
-            String columnName = metaData.getColumnName(i);
-//            metaData
+        for (int i = 0; i < columnCount; i++) {
+
+            try {
+                Field columnName = metaData.getClass().getDeclaredField("columnName");
+                columnName.setAccessible(true);
+                Object columnNames = columnName.get(metaData);
+                if (columnNames instanceof Collection) {
+                    Collection names = (Collection) columnNames;
+                    Collection<String> collection = (Collection<String>) columnNames.getClass().newInstance();
+                    names.forEach(e -> {
+                        collection.add(FieldNameUtils.underline2Camel(e.toString(), true));
+
+                    });
+                    columnName.set(metaData, collection);
+                }
+            } catch (NoSuchFieldException | IllegalAccessException | InstantiationException e) {
+                e.printStackTrace();
+            }
+
         }
         return resultSets;
     }
